3b6f40cacadd28e5e5811c3d8fcc7b8583ac1ddd
unknown <svclennan@icloud.com>
2020-01-14 13:22:28 -0600

    init

---
+++ Battleship/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Example of empty board with row and column labels
+            //string[,] board = new string[20, 20];
+            //int count = 1;
+            //char columns = Convert.ToChar(64);
+            //for (int k = 0; k <= board.GetLength(0); k++)
+            //{
+            //    if (k == 0)
+            //    {
+            //        Console.Write("    ");
+            //    }
+            //    else
+            //    {
+            //        Console.Write(" " + columns + " ");
+            //    }
+            //    columns++;
+            //}
+            //Console.WriteLine();
+            //for (int i = 0; i < board.GetLength(0); i++)
+            //{
+            //    if (count < 10)
+            //    {
+            //        Console.Write(" " + count + "  ");
+            //    }
+            //    else
+            //    {
+            //        Console.Write(" " + count + " ");
+            //    }
+            //    for (int j = 0; j < board.GetLength(1); j++)
+            //    {
+            //        board[i, j] = " ";
+            //        Console.Write($"[{board[i, j]}]");
+            //    }
+            //    count++;
+            //    Console.WriteLine();
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
